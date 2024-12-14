7ea960c9efbcdc58a519d292094c3348ead3ee03
youssef665 <110295462+youssef665@users.noreply.github.com>
2024-12-14 21:09:34 +0200

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open System.Windows.Forms
+open ContactManagementSystem
+
+[<EntryPoint>]
+let main _ =
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault(false)
+    let form = UI.createForm()
+    Application.Run(form)
+    0
+
